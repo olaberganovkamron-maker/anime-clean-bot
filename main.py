aa8f5b4a57c1d0f6b75a98fc364112c74c8bbf1c
olaberganovkamron-maker <olaberganovkamron@gmail.com>
2026-06-13 20:37:27 +0500

    main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-async def main():
-    # Eski so‘rovlarni tozalash (ConflictError ni yo‘qotadi)
-    await bot.delete_webhook(drop_pending_updates=True)
-    print("Bot ishga tushdi...")
-    await dp.start_polling(bot)
